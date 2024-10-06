4805af7215c372e84a8909060faf90c280dbadd4
Effie Armony <harmony.effie@gmail.com>
2024-10-06 22:57:01 +0300

    fix bug & add enter to input

--- js/country.controller.js
+++ js/country.controller.js
@@ -5,18 +5,22 @@ function onInit() {
 }
 
 function renderInfo(data) {
-    document.querySelector('.country-name').innerHTML = data.name.common
-    document.querySelector('.country-flag').src = data.flags.png
-    document.querySelector('.country-flag').alt = data.flags.alt
-    document.querySelector('.population').innerHTML = `population: ${data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
-    document.querySelector('.area').innerHTML = `area: ${data.area} km²`
-    console.log(data)
+  document.querySelector('.country-name').innerHTML = data.name.common
+  document.querySelector('.country-flag').src = data.flags.png
+  document.querySelector('.country-flag').alt = data.flags.alt
+  document.querySelector('.population').innerHTML = `population: ${data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
+  document.querySelector('.area').innerHTML = `area: ${data.area} km²`
+  console.log(data)
 }
 
 function onGetCountryInfo() {
-    let countryName = document.querySelector('.search-box').value
-    if(!countryName) countryName = 'israel'
-    getCountryByName(countryName)
+  let countryName = document.querySelector('.search-box').value
+  if (!countryName) countryName = 'israel'
+  getCountryByName(countryName)
     .then(res => renderInfo(res.data[0]))
     .catch(err => console.error(err))
 }
+
+document.querySelector('.search-box').addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') onGetCountryInfo()
+})
