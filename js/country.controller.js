2e2fea81a865896a1ae17e9f6e4ff5ac37b15535
Effie Armony <harmony.effie@gmail.com>
2024-10-06 18:07:08 +0300

    Add basic functionality

--- js/country.controller.js
+++ js/country.controller.js
@@ -1,9 +1,13 @@
 'use strict'
 
+let gValue = 'Israel'
+
 function onGetCountryInfo() {
-    console.log('Hi')
+    gValue = document.querySelector('.search-box').value
+    axios.get(`https://restcountries.com/v3.1/name/${gValue}`).then(res => renderInfo(res.data[0]))
 }
 
 function renderInfo(data) {
-    console.log('Rendering...')
+    document.querySelector('.pre').innerHTML = JSON.stringify(data, null, 2)
+    console.log(data)
 }
